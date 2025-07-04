4c02fdeecbbdfeaaa62b264bd32b71d60387a52e
guilhermededeus <dedeus.gomes@outlook.com>
2025-07-03 23:15:21 -0300

    Aula do dia 03/07/2025

---
+++ atividades/atividade_10/main.py
@@ -0,0 +1,20 @@
+"""
+# TODO - atividade: faça um CRUD (Create, Read, Update, Delete) em um JSON.
+Opções do menu:
+- Criar novo arquivo JSON (usuário irá informar o diretório).
+- Abrir arquivo JSON (usuário irá informar o diretório).
+- Cadastrar novo usuário em um JSON.
+- Listar todos os usuários de um arquivo JSON.
+- Pesquisar usuário através do valor de uma chave em um arquivo JSON.
+- Alterar dados de um usuário em um arquivo JSON.
+- Deletar usuário de um arquivo JSON.
+- Sair do programa
+Dados do usuário:
+- Nome
+- Data de nascimento
+- CPF
+- E-mail
+- Telefone
+- Filme favorito do usuário
+# NOTE - Mostre o nome do arquivo e o diretório no menu
+"""
