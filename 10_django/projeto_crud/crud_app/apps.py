6acd0a9cc4d7211560474109fccdfc151b7419c6
guilhermededeus <dedeus.gomes@outlook.com>
2025-09-19 23:19:09 -0300

    Aula do dia 19/09/2025

--- 10_django/projeto_crud/crud_app/apps.py
+++ 10_django/projeto_crud/crud_app/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class CrudPyConfig(AppConfig):
+class CrudAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'crud_py'
+    name = 'crud_app'
--- 10_django/projeto_crud/projeto_crud/settings.py
+++ 10_django/projeto_crud/projeto_crud/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5l6-0p3o2(cdycc_0a4#g(fxo*873o!6onoyeggom$c@=u5dj^'
+SECRET_KEY = 'django-insecure-_t%$et!t2c0e8s=lmp0p)-t&^zr794yuc$l6zppjnbn3r^sb1w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -38,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'crud_app'
-] 
+]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -80,6 +81,7 @@ DATABASES = {
     }
 }
 
+
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
@@ -120,3 +122,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
